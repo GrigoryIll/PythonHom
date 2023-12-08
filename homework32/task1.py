a9e279b7547e34c6f5a611aa5eed93e261d4c42f
Grigory <grigory@i.ru>
2023-12-09 00:44:55 +0300

    дз

--- homework32/task1.py
+++ homework32/task1.py
@@ -12,7 +12,7 @@
 
 def bin_search(array, num):
     start = 0
-    end = len(array)
+    end = len(array)-1
     while start <= end:
         mid = (start + end) // 2
         if num == array[mid]:
@@ -21,6 +21,7 @@ def bin_search(array, num):
             end = mid - 1
         if num > array[mid]:
             start = mid + 1
+    return -1
 
 
-print(bin_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10))
+print(bin_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 100))
